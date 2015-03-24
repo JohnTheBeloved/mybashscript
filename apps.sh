10d645388f0a1659c4198d99065bbffe530ab140
John Alade <john.alade@interswitchng.com>
2015-03-24 22:19:34 +0000

    Added apps.sh

---
+++ apps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Work scripts
+
+alias start_smartinsure_retailclient="node /home/johnthebeloved/development/project/nodejs/smartinsureRetailClient/index.js"
+
+alias start_smartinsure_tomcat="/opt/manualInstalls/appservers/tomcat_instances/smartinsure/bin/./startup.sh"
+
+alias stop_smartinsure_tomcat="/opt/manualInstalls/appservers/tomcat_instances/smartinsure/bin/./shutdown.sh"
+
+alias monitor_smartinsure_tomcat="tail -f /opt/manualInstalls/appservers/tomcat_instances/smartinsure/logs/*.*"
+
+alias edit_apps_init="nano ~/mybash/apps.sh"
